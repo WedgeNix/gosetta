edc46acdc5ef8b9b9ad7775846283325bbb0901b
PǝRV <alexander.w.matulionis@gmail.com>
2017-08-14 07:22:19 -0700

    Fixed constructor

--- gosetta.go
+++ gosetta.go
@@ -1,8 +1,20 @@
 package gosetta
 
+import (
+	"errors"
+	"os"
+)
+
+// Rose holds translation information.
 type Rose struct {
+	apiKey string
 }
 
-func New() *Rose {
-  return &Rose{}
+// New creates a rose for translating.
+func New() (*Rose, error) {
+	apiKey := os.Getenv("TRANSLATE_API_KEY")
+	if len(apiKey) < 1 {
+		return nil, errors.New("missing TRANSLATE_API_KEY")
+	}
+	return &Rose{apiKey}, nil
 }
